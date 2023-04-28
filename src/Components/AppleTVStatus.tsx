1a8b014162ba3078ade0321e1dec4530dc7d818b
Peter Jones <hi@peterjones.io>
2023-04-28 23:17:01 +0100

    code improvements, update media display

--- src/Components/AppleTVStatus.tsx
+++ src/Components/AppleTVStatus.tsx
@@ -1,6 +1,6 @@
 import { callService, subscribeEntities } from "home-assistant-js-websocket";
 import { Connection } from "home-assistant-js-websocket/dist/connection";
-import React, { useEffect, useState } from "react";
+import React, { CSSProperties, useEffect, useState } from "react";
 
 interface AppleTVStatusProps {
   connection: Connection;
@@ -12,10 +12,15 @@ interface AppCardProps {
   launchCommand: () => void;
 }
 
-interface MediaPlayerAttributes {
-  media_title: string;
-  entity_picture: string;
-}
+const avatarStyle: CSSProperties = {
+  width: "48px",
+  height: "48px",
+  borderRadius: "50%",
+  marginRight: "8px",
+  overflow: "hidden",
+  display: "inline-block",
+  verticalAlign: "middle",
+};
 
 const AppCard: React.FC<AppCardProps> = ({
   title,
@@ -24,11 +29,12 @@ const AppCard: React.FC<AppCardProps> = ({
 }) => (
   <div className="app-card" onClick={launchCommand}>
     <img src={imageUrl} alt={title} />
-    {/* <h4>{title}</h4> */}
   </div>
 );
 
-const AppleTVStatus: React.FC<AppleTVStatusProps> = ({ connection }) => {
+const LIVING_ROOM_ENTITY_ID = "media_player.living_room";
+
+const AppleTVStatus = ({ connection }: AppleTVStatusProps) => {
   const [currentlyPlaying, setCurrentlyPlaying] = useState<string | null>(null);
   const [currentlyPlayingMedia, setCurrentlyPlayingMedia] = useState<
     string | null
@@ -36,14 +42,10 @@ const AppleTVStatus: React.FC<AppleTVStatusProps> = ({ connection }) => {
 
   useEffect(() => {
     const unsubscribeEntities = subscribeEntities(connection, (entities) => {
-      const mediaPlayer = entities["media_player.living_room"];
+      const mediaPlayer = entities[LIVING_ROOM_ENTITY_ID];
       console.log(mediaPlayer.attributes);
-      if (mediaPlayer) {
-        setCurrentlyPlaying(mediaPlayer.attributes.media_title);
-        if (mediaPlayer.attributes.entity_picture) {
-          setCurrentlyPlayingMedia(mediaPlayer.attributes.entity_picture);
-        }
-      }
+      setCurrentlyPlaying(mediaPlayer?.attributes.media_title || null);
+      setCurrentlyPlayingMedia(mediaPlayer?.attributes.entity_picture || null);
     });
 
     return () => {
@@ -53,46 +55,62 @@ const AppleTVStatus: React.FC<AppleTVStatusProps> = ({ connection }) => {
 
   const launchApp = (source: string) => {
     callService(connection, "media_player", "select_source", {
-      entity_id: "media_player.living_room",
+      entity_id: LIVING_ROOM_ENTITY_ID,
       source: source,
     });
   };
 
-  const powerOnMediaPlayer = async () => {
-    if (connection) {
-      callService(connection, "media_player", "turn_on", {
-        entity_id: "media_player.living_room",
-      });
-    }
+  const powerOnMediaPlayer = () => {
+    callService(connection, "media_player", "turn_on", {
+      entity_id: LIVING_ROOM_ENTITY_ID,
+    });
   };
 
-  const powerOffMediaPlayer = async () => {
-    if (connection) {
-      callService(connection, "media_player", "turn_off", {
-        entity_id: "media_player.living_room",
-      });
-    }
+  const powerOffMediaPlayer = () => {
+    callService(connection, "media_player", "turn_off", {
+      entity_id: LIVING_ROOM_ENTITY_ID,
+    });
   };
+
+  const apps = [
+    {
+      title: "Netflix",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
+    },
+    {
+      title: "Disney+",
+      imageUrl:
+        "https://static-assets.bamgrid.com/product/disneyplus/images/logo.1a56f51c764022ee769c91d894d44326.svg",
+    },
+  ];
 
   return (
     <div className="apple-tv-status">
       <button onClick={powerOnMediaPlayer}>Turn on</button>
+      <button onClick={powerOffMediaPlayer}>Turn off</button>
       <h2>Apple TV Status</h2>
-      <p>Currently playing: {currentlyPlaying || "Nothing"}</p>
-      {currentlyPlayingMedia && (
-        <img src={process.env.REACT_APP_HASS_URL + currentlyPlayingMedia} />
-      )}
+      <div style={{ display: "flex", alignItems: "center" }}>
+        {currentlyPlayingMedia && (
+          <div style={avatarStyle}>
+            <img
+              src={process.env.REACT_APP_HASS_URL + currentlyPlayingMedia}
+              alt={currentlyPlaying || "Now playing"}
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          </div>
+        )}
+        <p>{currentlyPlaying}</p>
+      </div>
       <div className="app-grid">
-        <AppCard
-          title="Netflix"
-          imageUrl="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-          launchCommand={() => launchApp("Netflix")}
-        />
-        <AppCard
-          title="Disney+"
-          imageUrl="https://static-assets.bamgrid.com/product/disneyplus/images/logo.1a56f51c764022ee769c91d894d44326.svg"
-          launchCommand={() => launchApp("Disney+")}
-        />
+        {apps.map((app) => (
+          <AppCard
+            key={app.title}
+            title={app.title}
+            imageUrl={app.imageUrl}
+            launchCommand={() => launchApp(app.title)}
+          />
+        ))}
       </div>
     </div>
   );
