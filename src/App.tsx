5a250ef45571b01149c1572d4fd812b9475f0bc5
Peter Jones <hi@peterjones.io>
2023-04-29 09:56:10 +0100

    add styled components and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,18 @@ import {
   subscribeEntities,
 } from "home-assistant-js-websocket";
 import { useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 import { fetchUser, getAuthAndConnection } from "./api";
 import "./App.css";
 import AppleTVStatus from "./Components/AppleTVStatus";
 import BottomNav from "./Components/BottomNav";
 import PS5Status from "./Components/PS5Status";
 import VolumeSlider from "./Components/VolumeSlider";
+
+const StyledButton = styled.button`
+  ${tw`bg-blue-500 text-white font-bold py-2 px-4 rounded`}
+`;
 
 const App = () => {
   const [connection, setConnection] = useState<Connection | null>(null);
@@ -89,6 +95,8 @@ const App = () => {
 
   return (
     <div className="App">
+      {/* test styled components and tailwind */}
+      <StyledButton>Click me</StyledButton>
       <button onClick={powerOnTV}>powerOnTV</button>
       <div className="container">
         {isMediaPlayerOn && (
