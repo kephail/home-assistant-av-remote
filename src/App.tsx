bda5aaa56b3b4c42a04ac9f64a8b2c77d07e1c64
Peter Jones <hi@peterjones.io>
2023-04-29 15:16:12 +0100

    toggle power of tv and av processor

--- src/App.tsx
+++ src/App.tsx
@@ -27,13 +27,16 @@ const BottomContainer = styled.div`
   ${tw`fixed bottom-0 left-0 right-0`}
 `;
 
-const PowerOffButton = styled.div`
-  ${tw` top-4 right-4 text-red-500 fixed rounded-full p-1 border border-black`}
+const TogglePowerButton = styled.div`
+  ${tw`top-4 right-4 fixed rounded-full p-1 border border-black`}
+  ${({ isOn }: { isOn: Boolean }) =>
+    isOn ? tw`text-red-500` : tw`text-green-500`}
 `;
 
 const App = () => {
   const [connection, setConnection] = useState<Connection | null>(null);
   const [isMediaPlayerOn, setIsMediaPlayerOn] = useState<boolean>(false);
+  const [isTVOn, setIsTVOn] = useState<boolean>(false);
   const [selectedSource, setSelectedSource] = useState<string | null>(null);
 
   useEffect(() => {
@@ -46,11 +49,15 @@ const App = () => {
 
         subscribeEntities(connection, (entities) => {
           const mediaPlayer = entities["media_player.anthem_av"];
+          const tv = entities["media_player.83g2"];
           if (mediaPlayer) {
             setSelectedSource(mediaPlayer.attributes.source);
             setIsMediaPlayerOn(
               mediaPlayer.state === "on" || mediaPlayer.state === "playing"
             );
+          }
+          if (tv) {
+            setIsTVOn(tv.state === "on" || tv.state === "playing");
           }
         });
       } catch (err) {
@@ -67,13 +74,13 @@ const App = () => {
     }
   }, [connection]);
 
-  const powerOnTV = async () => {
+  const powerOnTV = useCallback(async () => {
     if (connection) {
       callService(connection, "wake_on_lan", "send_magic_packet", {
         mac: process.env.REACT_APP_WEBOS_TV_MAC_ADDRESS,
       });
     }
-  };
+  }, [connection]);
 
   const powerOffMediaPlayer = useCallback(async () => {
     if (connection) {
@@ -82,6 +89,24 @@ const App = () => {
       });
     }
   }, [connection]);
+
+  const togglePower = useCallback(async () => {
+    if (connection) {
+      const mediaPlayerService = isMediaPlayerOn ? "turn_off" : "turn_on";
+
+      callService(connection, "media_player", mediaPlayerService, {
+        entity_id: "media_player.anthem_av",
+      });
+
+      if (isTVOn) {
+        callService(connection, "media_player", "turn_off", {
+          entity_id: "media_player.83g2",
+        });
+      } else {
+        powerOnTV();
+      }
+    }
+  }, [connection, isMediaPlayerOn, isTVOn, powerOnTV]);
 
   const renderComponentBySource = () => {
     if (!connection) return null;
@@ -108,11 +133,14 @@ const App = () => {
     <AppContainer>
       <button onClick={powerOnTV}>powerOnTV</button>
       <ContentContainer>
-        {isMediaPlayerOn && (
-          <PowerOffButton onClick={powerOffMediaPlayer} color="secondary">
-            <PowerSettingsNewIcon />
-          </PowerOffButton>
-        )}
+        <TogglePowerButton
+          isOn={isMediaPlayerOn && isTVOn}
+          onClick={togglePower}
+          color="secondary"
+        >
+          <PowerSettingsNewIcon />
+        </TogglePowerButton>
+
         {renderComponentBySource()}
       </ContentContainer>
       {isMediaPlayerOn && (
